da3af34ae6f24f105b0464b19e413b76dc5bd1a6
Wei Kang <weikangchia@gmail.com>
2019-08-23 00:54:27 +0800

    Update uploadUsers API Add try catch and return appropriate API response

--- src/main/java/com/cdit/challenge/controller/UserController.java
+++ src/main/java/com/cdit/challenge/controller/UserController.java
@@ -8,13 +8,14 @@ import com.cdit.challenge.util.UserUtil;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
@@ -35,11 +36,19 @@ public class UserController {
     }
 
     @RequestMapping(value = "/users/upload", method = RequestMethod.POST)
-    public void uploadUsers(@RequestParam("file") MultipartFile userUploadFile) throws IOException {
+    public ResponseEntity uploadUsers(@RequestParam("file") MultipartFile userUploadFile) {
         if (!userUploadFile.isEmpty()) {
-            CSVParser userParseResults = CSVFormat.EXCEL.withHeader().parse(new InputStreamReader((userUploadFile.getInputStream())));
-            List<User> users = UserUtil.parseCsvResults(userParseResults);
-            userRepository.saveAll(users);
+            try {
+                CSVParser userParseResults = CSVFormat.EXCEL.withHeader().parse(new InputStreamReader((userUploadFile.getInputStream())));
+                List<User> users = UserUtil.parseCsvResults(userParseResults);
+                userRepository.saveAll(users);
+            } catch (Exception ex) {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
 }

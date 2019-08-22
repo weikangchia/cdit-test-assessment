34f4d0f5c577029d8ac4b81f34192050c83fd3c5
Wei Kang <weikangchia@gmail.com>
2019-08-22 21:54:12 +0800

    Add UserRepository

---
+++ src/main/java/com/cdit/challenge/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cdit.challenge.repository;
+
+import com.cdit.challenge.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
+}
